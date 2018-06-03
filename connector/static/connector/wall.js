a24822a187bef3ada58df19208e9ca017a1b14e5
Rocky Li <zl131@duke.edu>
2018-06-03 18:06:36 +0800

    Created Pings using ajax

---
+++ connector/static/connector/wall.js
@@ -0,0 +1,24 @@
+// Ajax call to ping websites.
+$('.ajaxcall').click(function(event) {
+    event.preventDefault();
+    console.log("executing ajax call");
+    $.ajax({
+        url: $(this).attr('href'),
+        context: this,
+        success: function(response) {
+          console.log('I have received a response');
+          var date = new Date(response['sent_time']);
+          var datestr = date.toLocaleString();
+
+          if (response['received'] > 0){
+            $(this).parent().parent().find("p.results").html("Pinged at " + datestr + '<br />' + response['received'] + "/5 Packets Received <br /> Average RTT: " + response['average'] + ' ms');
+          } else {
+            $(this).parent().parent().find("p.results").html("Pinged at " + datestr + '<br /> 0/5 Packets Received <br /> Average RTT: N/A');
+          }
+          console.log($(this).parent().parent().find("p.results"));
+          console.log(datestr);
+        }
+    });
+    console.log("Ajax call finished");
+    return false; // for good measure
+});
--- connector/views.py
+++ connector/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Website, Result
 from django.utils import timezone
 import subprocess
@@ -8,23 +8,44 @@ import subprocess
 def index(request):
     website_list = Website.objects.all()
     for each in website_list:
+        each.condensed_name = each.website_name.replace(" ", "")
         if len(each.result_set.all()) > 0:
             each.result = each.result_set.order_by('-ping_time')[0]
         else:
             pingwebsite(each)
             each.result = each.result_set.order_by('-ping_time')[0]
+        if each.result.ping_result == -1:
+            each.result.ping_result = "N/A"
+        else:
+            each.result.ping_result = str(each.result.ping_result)
+            each.result.ping_result += " ms"
+        if each.result.ping_success == 5:
+            each.result.color = "bg-lime"
+        elif each.result.ping_success > 0:
+            each.result.color = "bg-yellow"
+        else:
+            each.result.color = "bg-red"
     context = {
     'website_list': website_list
     }
     return render(request, 'connector/index.html', context)
 
-def ping(request, website):
-    pass
+def ping(request, website_id):
+    my_website = Website.objects.get(pk=website_id)
+    sent, returns, avg, curr_time = pingwebsite(my_website)
+    resdata = {
+    'received' : returns,
+    'average' : avg,
+    'sent_time' : curr_time
+    }
+    return JsonResponse(resdata)
 
 def pingwebsite(website):
     url = website.website_url
     sent, returns, avg = pingfunction(url)
-    website.result_set.create(ping_time=timezone.now(), ping_success=returns, ping_total=sent, ping_result=avg)
+    curr_time = timezone.now()
+    website.result_set.create(ping_time=curr_time, ping_success=returns, ping_total=sent, ping_result=avg)
+    return sent, returns, avg, curr_time
 
 def pingfunction(url):
     result = subprocess.run(['ping', '-c 5', '-W 2', url], stdout=subprocess.PIPE)
